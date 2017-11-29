0fb48b6a912bcb01f5683d965aa5598bb4b64835
Gregory Amerson <gregory.amerson@liferay.com>
2017-11-29 10:46:01 -0600

    improve test
    Signed-off-by: Gregory Amerson <gregory.amerson@liferay.com>

--- src/test/java/aQute/jpm/main/ServiceTest.java
+++ src/test/java/aQute/jpm/main/ServiceTest.java
@@ -1,5 +1,8 @@
 package aQute.jpm.main;
 
+import java.io.UnsupportedEncodingException;
+
+import aQute.jpm.lib.Service;
 import junit.framework.TestCase;
 
 public class ServiceTest extends TestCase {
@@ -9,20 +12,54 @@ public class ServiceTest extends TestCase {
 		removeTestService();
 	}
 
-	public void testService() throws Exception {
-		Main.main(new String[] {
-			"-etu", "service", "--create", "target/test-classes/biz.aQute.jpm.daemon.jar", "dm"
-		});
-	}
-
 	@Override
 	protected void tearDown() throws Exception {
 		removeTestService();
 	}
 
+	public void testServiceCreate() throws Exception {
+		MainNoFail jpm = new MainNoFail();
+
+		jpm.run(new String[] {
+			"-etu", "service", "--create", "target/test-classes/biz.aQute.jpm.daemon.jar", "dm"
+		});
+
+		Service service = jpm.jpm.getService("dm");
+
+		assertNotNull(service);
+	}
+
 	private void removeTestService() throws Exception {
-		Main.main(new String[] {
+		MainNoFail jpm = new MainNoFail();
+
+		jpm.run(new String[] {
 			"-etu", "service", "--remove", "dm"
 		});
+
+		Service service = jpm.jpm.getService("dm");
+
+		assertNull(service);
+	}
+
+	private static class MainNoFail extends Main {
+
+		public MainNoFail() throws UnsupportedEncodingException {
+			super();
+		}
+
+		@Override
+		public boolean check(String... pattern) {
+			return true;
+		}
+
+		@Override
+		public void run(String[] args) throws Exception {
+			try {
+				super.run(args);
+			} finally {
+				err.flush();
+				out.flush();
+			}
+		}
 	}
 }
